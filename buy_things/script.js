3fbe0f0129c9be0a7e7a1cb9600ca598e8e4bbeb
Lucy Johnston <ljohnston931@gmail.com>
2018-03-01 08:40:17 -0700

    Finished styles

--- buy_things/script.js
+++ buy_things/script.js
@@ -4,7 +4,14 @@ var app = new Vue({
         products: [
             {text:"Fancy Dog", price:500, selected:false},
             {text:"Superpowers", price:1000000, selected:false},
-            {text:"Used Paper Towel", price:1, selected:false}
+            {text:"Used Paper Towel", price:1, selected:false},
+            {text:"Tom Cruise's Hair Clippings", price:200,selected:false},
+            {text:"Antique Pizza Slice", price:22, selected:false},
+            {text:"Whole Cello",price:4300,selected:false},
+            {text:"Half Cello",price:15,selected:false},
+            {text:"Toothpaste",price:2,selected:false},
+            {text:"Dad's Fingernail Clippings",price:8,selected:false},
+            {text:"Personal Chef",price:5500,selected:false}
         ],
         total: 0,
     },
@@ -14,6 +21,15 @@ var app = new Vue({
                 return item.selected;
             });
         },
+        somethingSelected: function() {
+            for(var i=0; i < this.products.length; i++) {
+                if (this.products[i].selected)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     },
     methods: {
         selectItem: function(item) {
@@ -21,6 +37,7 @@ var app = new Vue({
             {
                 this.total += item.price;
                 item.selected = true;
+
             }
             else //uncheck
             {
@@ -28,5 +45,14 @@ var app = new Vue({
                 item.selected = false;
             }
         },
+        pay: function() {
+            location.href="./money.html";
+        },
+       /* background: function() {
+            var backgrounds = new Array[
+                "url('/images/m1.gif')"
+            ]
+            document.getElementById("money").style.backgroundImage=backgrounds[0];
+        }*/
     }
 });
